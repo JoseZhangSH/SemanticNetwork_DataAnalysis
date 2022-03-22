38f38be6582773984900bba5fe6ec89b4e68fcf7
JoseZhangSH <josezhangsh@gmail.com>
2022-03-22 22:54:56 +0800

    Update: more test API
    Update: more test API

--- data/flask_test.py
+++ data/flask_test.py
@@ -70,29 +70,78 @@ def Experiment_Progress():
 
 
 
-@app.route("/shortestPath/<concept>",methods=['GET','POST'])
+@app.route("/therapyRecord",methods=['GET','POST'])
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
-def Shortest_Path(concept):
-    target_maincategory = df_complete[df_complete['Concept']==concept]['上级类别'].unique()[0]
+def Therapy_Record():
+    therapy_record = {
+        "TherapyRecord":[
+            {
+            "id":"拖鞋",
+            "PictureNaming":[{
+                "result":"false",
+                "time":20.0,
+                "date":"2022-02-28"
+            },{
+                "result":"true",
+                "time":3.2,
+                "date":"2022-03-12"
+            },],
+            "SFA":[{
+                "reaction":[{
+                    "relationship":"属于",
+                    "feature":"交通工具",
+                },{
+                    "relationship":"能够",
+                    "feature":"驾驶",
+                },],
+                "date":"2022-02-28"
+            },{
+                "reaction":[{
+                    "relationship":"属于",
+                    "feature":"交通工具",
+                },{
+                    "relationship":"能够",
+                    "feature":"驾驶",
+                },],
+                "date":"2022-02-28"
+            },],
+            "SFAPlan":"true",
+            },{
+            "id":"鞋垫",
+            "PictureNaming":[{
+                "result":"false",
+                "time":20.0,
+                "date":"2022-02-28"
+            },{
+                "result":"true",
+                "time":3.2,
+                "date":"2022-03-12"
+            },],
+            "SFA":[{
+                "reaction":[{
+                    "relationship":"属于",
+                    "feature":"交通工具",
+                },{
+                    "relationship":"能够",
+                    "feature":"驾驶",
+                },],
+                "date":"2022-02-28"
+            },{
+                "reaction":[{
+                    "relationship":"属于",
+                    "feature":"交通工具",
+                },{
+                    "relationship":"能够",
+                    "feature":"驾驶",
+                },],
+                "date":"2022-02-28"
+            },],
+            "SFAPlan":"false",
+            }
+        ]
+    }
+    return therapy_record
 
-    nodes = []
-    for n in G_MasterConceptNetwork.nodes:
-        if 'picture_naming_result' in G_MasterConceptNetwork.nodes[n]:
-            if (G_MasterConceptNetwork.nodes[n]['maincategory']==target_maincategory) & (G_MasterConceptNetwork.nodes[n]['picture_naming_result']==True):
-                nodes.append(n)
-
-    shortest_length = 1000
-    shortest_path = []
-
-    for node in nodes:
-        path = nx.dijkstra_path(G_MasterConceptNetwork, node, concept)
-        if(len(path)<shortest_length):
-            shortest_length = len(path)
-            shortest_path = path
-        subgraph = G_MasterConceptNetwork.subgraph(shortest_path)
-
-
-    return json.dumps(json_graph.node_link_data(subgraph),ensure_ascii=False).replace('links','edges')
 
 
 @app.route("/therapyPlan/<para>",methods=['GET','POST'])
@@ -840,3 +889,354 @@ def Therapy_Plan(para):
         return plan_a
     else:
         return plan_b
+
+
+@app.route("/shortestPath/<concept>",methods=['GET','POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def Shortest_Path(concept):
+    target_maincategory = df_complete[df_complete['Concept']==concept]['上级类别'].unique()[0]
+
+    nodes = []
+    for n in G_MasterConceptNetwork.nodes:
+        if 'picture_naming_result' in G_MasterConceptNetwork.nodes[n]:
+            if (G_MasterConceptNetwork.nodes[n]['maincategory']==target_maincategory) & (G_MasterConceptNetwork.nodes[n]['picture_naming_result']==True):
+                nodes.append(n)
+
+    shortest_length = 1000
+    shortest_path = []
+
+    for node in nodes:
+        path = nx.dijkstra_path(G_MasterConceptNetwork, node, concept)
+        if(len(path)<shortest_length):
+            shortest_length = len(path)
+            shortest_path = path
+        subgraph = G_MasterConceptNetwork.subgraph(shortest_path)
+
+
+    return json.dumps(json_graph.node_link_data(subgraph),ensure_ascii=False).replace('links','edges')
+
+
+@app.route("/nodeRecord/<concept>",methods=['GET','POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def Node_Record(concept):
+    node_record = {
+        "id":concept,
+        "PictureNaming":[{
+            "result":"false",
+            "time":20.0,
+            "date":"2022-02-28"
+        },{
+            "result":"true",
+            "time":3.2,
+            "date":"2022-03-12"
+        },],
+        "SFA":[{
+            "reaction":[{
+                "relationship":"属于",
+                "feature":"交通工具",
+            },{
+                "relationship":"能够",
+                "feature":"驾驶",
+            },],
+            "date":"2022-02-28"
+        },{
+            "reaction":[{
+                "relationship":"属于",
+                "feature":"交通工具",
+            },{
+                "relationship":"能够",
+                "feature":"驾驶",
+            },],
+            "date":"2022-02-28"
+        }
+        ]
+    }
+    return node_record
+
+
+
+@app.route("/planGraph",methods=['GET','POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def Plan_Graph():
+    plan_graph = {
+        "nodes": [{
+            "subcategory": "工具",
+            "maincategory": "人造物",
+            "id": "钢笔"
+        }, {
+            "subcategory": "工具",
+            "maincategory": "人造物",
+            "id": "画笔"
+        }, {
+            "subcategory": "工具",
+            "maincategory": "人造物",
+            "id": "画纸"
+        }, {
+            "subcategory": "工具",
+            "maincategory": "人造物",
+            "id": "记号笔"
+        }, {
+            "subcategory": "工具",
+            "maincategory": "人造物",
+            "id": "蜡笔"
+        }, {
+            "subcategory": "身体部位",
+            "maincategory": "身体部位",
+            "id": "手"
+        }, {
+            "subcategory": "身体部位",
+            "maincategory": "身体部位",
+            "id": "手臂"
+        }, {
+            "subcategory": "身体部位",
+            "maincategory": "身体部位",
+            "id": "手掌"
+        }, {
+            "subcategory": "身体部位",
+            "maincategory": "身体部位",
+            "id": "手指"
+        }, {
+            "subcategory": "身体部位",
+            "maincategory": "身体部位",
+            "id": "臀部"
+        }],
+        "edges": [{
+            "weight": 1.0,
+            "source": "钢笔",
+            "target": "钢笔"
+        }, {
+            "weight": 0.833563665,
+            "source": "钢笔",
+            "target": "画笔"
+        }, {
+            "weight": 0.729334454,
+            "source": "钢笔",
+            "target": "画纸"
+        }, {
+            "weight": 0.806783151,
+            "source": "钢笔",
+            "target": "记号笔"
+        }, {
+            "weight": 0.789160661,
+            "source": "钢笔",
+            "target": "蜡笔"
+        }, {
+            "weight": 0.461681547,
+            "source": "钢笔",
+            "target": "手"
+        }, {
+            "weight": 0.522326804,
+            "source": "钢笔",
+            "target": "手臂"
+        }, {
+            "weight": 0.452785838,
+            "source": "钢笔",
+            "target": "手掌"
+        }, {
+            "weight": 0.503370717,
+            "source": "钢笔",
+            "target": "手指"
+        }, {
+            "weight": 1.0,
+            "source": "画笔",
+            "target": "画笔"
+        }, {
+            "weight": 0.824119591,
+            "source": "画笔",
+            "target": "画纸"
+        }, {
+            "weight": 0.904647908,
+            "source": "画笔",
+            "target": "记号笔"
+        }, {
+            "weight": 0.868122534,
+            "source": "画笔",
+            "target": "蜡笔"
+        }, {
+            "weight": 0.524525185,
+            "source": "画笔",
+            "target": "手"
+        }, {
+            "weight": 0.594872923,
+            "source": "画笔",
+            "target": "手臂"
+        }, {
+            "weight": 0.513030702,
+            "source": "画笔",
+            "target": "手掌"
+        }, {
+            "weight": 0.577260057,
+            "source": "画笔",
+            "target": "手指"
+        }, {
+            "weight": 1.0,
+            "source": "画纸",
+            "target": "画纸"
+        }, {
+            "weight": 0.740142568,
+            "source": "画纸",
+            "target": "记号笔"
+        }, {
+            "weight": 0.913701008,
+            "source": "画纸",
+            "target": "蜡笔"
+        }, {
+            "weight": 0.540371419,
+            "source": "画纸",
+            "target": "手"
+        }, {
+            "weight": 0.577401642,
+            "source": "画纸",
+            "target": "手臂"
+        }, {
+            "weight": 0.532365153,
+            "source": "画纸",
+            "target": "手掌"
+        }, {
+            "weight": 0.571582634,
+            "source": "画纸",
+            "target": "手指"
+        }, {
+            "weight": 1.0,
+            "source": "记号笔",
+            "target": "记号笔"
+        }, {
+            "weight": 0.806684534,
+            "source": "记号笔",
+            "target": "蜡笔"
+        }, {
+            "weight": 0.460387208,
+            "source": "记号笔",
+            "target": "手"
+        }, {
+            "weight": 0.499090958,
+            "source": "记号笔",
+            "target": "手臂"
+        }, {
+            "weight": 0.453705377,
+            "source": "记号笔",
+            "target": "手掌"
+        }, {
+            "weight": 0.500626342,
+            "source": "记号笔",
+            "target": "手指"
+        }, {
+            "weight": 1.0,
+            "source": "蜡笔",
+            "target": "蜡笔"
+        }, {
+            "weight": 0.563729515,
+            "source": "蜡笔",
+            "target": "手"
+        }, {
+            "weight": 0.605572089,
+            "source": "蜡笔",
+            "target": "手臂"
+        }, {
+            "weight": 0.555566419,
+            "source": "蜡笔",
+            "target": "手掌"
+        }, {
+            "weight": 0.600878023,
+            "source": "蜡笔",
+            "target": "手指"
+        }, {
+            "weight": 1.0,
+            "source": "手",
+            "target": "手"
+        }, {
+            "weight": 0.852983229,
+            "source": "手",
+            "target": "手臂"
+        }, {
+            "weight": 0.864065397,
+            "source": "手",
+            "target": "手掌"
+        }, {
+            "weight": 0.887756355,
+            "source": "手",
+            "target": "手指"
+        }, {
+            "weight": 0.585001422,
+            "source": "手",
+            "target": "臀部"
+        }, {
+            "weight": 1.0,
+            "source": "手臂",
+            "target": "手臂"
+        }, {
+            "weight": 0.883878504,
+            "source": "手臂",
+            "target": "手掌"
+        }, {
+            "weight": 0.916958628,
+            "source": "手臂",
+            "target": "手指"
+        }, {
+            "weight": 0.684903322,
+            "source": "手臂",
+            "target": "臀部"
+        }, {
+            "weight": 1.0,
+            "source": "手掌",
+            "target": "手掌"
+        }, {
+            "weight": 0.804232548,
+            "source": "手掌",
+            "target": "手指"
+        }, {
+            "weight": 0.625284889,
+            "source": "手掌",
+            "target": "臀部"
+        }, {
+            "weight": 1.0,
+            "source": "手指",
+            "target": "手指"
+        }, {
+            "weight": 0.625265234,
+            "source": "手指",
+            "target": "臀部"
+        }, {
+            "weight": 1.0,
+            "source": "臀部",
+            "target": "臀部"
+        }]
+    }
+
+    return plan_graph
+
+
+@app.route("/nodeInfo/<concept>",methods=['GET','POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def Node_Info(concept):
+    node_info={
+        "id":"摩托车",
+        "maincategory":"交通工具",
+        "subcategory":"车辆",
+        "features": {
+            "范畴": ["车", "船", "飞机"],
+            "用途": ["行驶", "乘坐", "开", "骑"],
+            "动作": ["司机", "维护", "修理", "加油"],
+            "属性": ["方向盘", "车身", "车厢", "仪表盘"],
+            "场所": [],
+            "联想": ["房屋", "货车", "轿车", "电动车"]
+        }
+    }
+    return node_info
+
+@app.route("/edgeInfo/<parameters>",methods=['GET','POST'])
+@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+def Edge_Info(parameters):
+    edge_info={
+        "source":"画笔",
+        "target":"手臂",
+        "shared_features": {
+            "范畴": ["车", "船", "飞机"],
+            "用途": ["行驶", "乘坐", "开", "骑"],
+            "动作": ["司机", "维护", "修理", "加油"],
+            "属性": ["方向盘", "车身", "车厢", "仪表盘"],
+            "场所": [],
+            "联想": ["房屋", "货车", "轿车", "电动车"]
+        }
+    }
+    return edge_info
